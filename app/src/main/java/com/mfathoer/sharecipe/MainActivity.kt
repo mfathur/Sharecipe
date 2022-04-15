90bfee49fd77f6a0c5aae60e1e94a6c6ace3ba24
M Fathur <mfathur17@gmail.com>
2022-04-15 22:51:13 +0700

    init navigation component, browsefragment,bookmarksfragment

--- app/src/main/java/com/mfathoer/sharecipe/MainActivity.kt
+++ app/src/main/java/com/mfathoer/sharecipe/MainActivity.kt
@@ -1,13 +1,32 @@
 package com.mfathoer.sharecipe
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.mfathoer.sharecipe.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this,R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+
+        navController = navHostFragment.navController
+
+//        synchronize bottom navigation with nav host fragment
+        NavigationUI.setupWithNavController(binding.bottomNav,navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
