4a3290d70668b648cbe87654effe9c1df51c2751
M Fathur <mfathur17@gmail.com>
2022-04-16 06:56:01 +0700

    init detailrecipefragment & create basefragment

---
+++ app/src/main/java/com/mfathoer/sharecipe/BaseFragment.kt
@@ -0,0 +1,41 @@
+package com.mfathoer.sharecipe
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.LayoutRes
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.Fragment
+
+abstract class BaseFragment<T : ViewDataBinding>(@LayoutRes private val layoutResId: Int) :
+    Fragment() {
+
+    private var _binding: T? = null
+
+    protected val binding get() = _binding!!
+
+    // init data binding
+    open fun T.initialize() {}
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = DataBindingUtil.inflate(inflater, layoutResId, container, false)
+
+        binding.lifecycleOwner = viewLifecycleOwner
+
+        binding.initialize()
+
+        // Inflate the layout for this fragment
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        _binding = null
+        super.onDestroyView()
+    }
+
+}
---
+++ app/src/main/java/com/mfathoer/sharecipe/DetailRecipeFragment.kt
@@ -0,0 +1,6 @@
+package com.mfathoer.sharecipe
+
+import com.mfathoer.sharecipe.databinding.FragmentDetailRecipeBinding
+
+class DetailRecipeFragment :
+    BaseFragment<FragmentDetailRecipeBinding>(R.layout.fragment_detail_recipe)
